355cb91b3d7cbcaffaaf6909d2b7befc22cad172
ahmed <ahmedafli739@gmail.com>
2026-03-08 20:13:45 +0100

    microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,15 @@ export default function RootLayout({
             gtag('config', 'G-SDJNW69WV6');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vsoxrfcjhk");
+          `}
+        </Script>
       </head>
       <body
         className={`${inter.variable} ${bebasNeue.variable} font-sans antialiased bg-black text-white`}
